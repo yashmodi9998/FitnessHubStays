1f6df2d88604f1afac5efd03a242e9b25c47083e
Yash Modi <Yashmodi998@gmail.com>
2024-03-16 15:25:24 -0400

    new

--- FitnessHubStays/Controllers/HomeController.cs
+++ FitnessHubStays/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace FitnessHubStays.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description pae.";
 
             return View();
         }
